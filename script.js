e7db3e125ff8e31204524aee1d2097625ebd37e4
Pragatiaboti <67864526+Pragatiaboti@users.noreply.github.com>
2020-07-27 17:40:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hello from script");
+function calljs() {
+  addRowToTable();
+};
+function signup()
+{
+  alert("Registered Sucessfully..Enjoy Music..");
+}
+
+
+https://pragatiaboti.github.io/registrationevent/.
